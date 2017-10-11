535015f8be3bad97f49679356f7cfb0bf574e999
Latebricole <latebricole@protonmail.com>
2017-10-11 15:06:30 +0100

    Removed frame

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function onClosed() {
 function createMainWindow() {
 	const win = new electron.BrowserWindow({
 		width: 600,
-		height: 400
+		height: 400,
+		frame: false,
 	});
 
 	win.loadURL(`file://${__dirname}/index.html`);
